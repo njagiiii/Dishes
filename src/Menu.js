887818876d6f27e6e4a8e946aff8e0910722e856
Abed Nganga <ngangaabed@gmail.com>
2023-08-01 15:27:25 +0300

    Styling Menu Cards

--- src/Menu.js
+++ src/Menu.js
@@ -24,15 +24,15 @@ const Menu = () => {
 
   return (
     <div className="container">
-      <div className="animate-text"> 
-        <p><h1>Welcome to Tasty Dishes</h1>Welcome to CJ's delicious universe. Discover the Delectable Delights on Our Exquisite Restaurant Menu! <br></br>Everything from our Big on Breakfast, Perfected Drinks, Decadent to your Generous Big Meals Right here at your fingertips.<br></br>ORDER NOW!</p>
-      </div>
+    <div className="animate-text"> 
+      <p><h1>Welcome to Tasty Dishes</h1>Welcome to CJ's delicious universe. Discover the Delectable Delights on Our Exquisite Restaurant Menu! <br></br>Everything from our Big on Breakfast, Perfected Drinks, Decadent to your Generous Big Meals Right here at your fingertips.<br></br>ORDER NOW!</p>
+    </div>
     <div>
           {/* Menu Row 1 */}
-          <div className="row mt-5">
+          <div className="row mt-5 ">
             {/* Row 1 Card 1 */}
             <div className="col">
-                <div class="card menu-card" style={{width: "30rem"}}>
+                <div class="card menu-card bg-light" style={{width: "30rem"}}>
                   <img src="https://cafejavasmedia.s3.af-south-1.amazonaws.com/categoryImages/1682056978.jpg" class="card-img-top" alt="..."></img>
                   <div class="card-body">
                     <h5 class="card-title">Big on Breakfast</h5>
@@ -44,7 +44,7 @@ const Menu = () => {
 
             {/* Row 1 Card 2 */}
             <div className="col">
-            <div class="card menu-card" style={{width: "30rem"}}>
+            <div class="card menu-card bg-light" style={{width: "30rem"}}>
                   <img src="https://images.pexels.com/photos/88917/pexels-photo-88917.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img-top" alt="..."></img>
                   <div class="card-body">
                     <h5 class="card-title">Appetizers</h5>
@@ -60,7 +60,7 @@ const Menu = () => {
           <div className="row mt-5">
             {/* Row 2 Card 1 */}
             <div className="col">
-            <div class="card menu-card" style={{width: "30rem"}}>
+            <div class="card menu-card bg-light" style={{width: "30rem"}}>
                   <img src="https://restaurentapp.s3.eu-west-1.amazonaws.com/categoryImages/1579274066.jpg" class="card-img-top" alt="..."></img>
                   <div class="card-body">
                     <h5 class="card-title">Desserts</h5>
@@ -72,7 +72,7 @@ const Menu = () => {
 
             {/* Row 2 Card 2 */}
             <div class="col">
-              <div class="card menu-card" style={{width: "30rem"}}>
+              <div class="card menu-card bg-light" style={{width: "30rem"}}>
                   <img src="https://images.pexels.com/photos/12842926/pexels-photo-12842926.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img-top" alt="..."></img>
                   <div class="card-body">
                     <h5 class="card-title">Main Courses</h5>
@@ -85,8 +85,8 @@ const Menu = () => {
           {/* Menu Row 3 */}
           <div className="row mt-5">
             {/* Row 3 Card 1 */}
-            <div className="col">
-            <div class="card menu-card" style={{width: "30rem"}}>
+            <div className="col d-flex justify-content-center">
+            <div class="card menu-card bg-light" style={{width: "30rem"}}>
                   <img src="https://images.pexels.com/photos/2789328/pexels-photo-2789328.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img-top" alt="..."></img>
                   <div class="card-body">
                     <h5 class="card-title">Drinks</h5>
