0b411241a0622fc2d3f89a14df81ab9ff5f5602f
cedric <cedric.michael@student.moringaschool.com>
2023-08-01 15:57:54 +0300

    added slider and images

--- src/Home.js
+++ src/Home.js
@@ -2,27 +2,24 @@ import React from 'react'
 
 const Home= () => {
   return (
-    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="true">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="https://media.istockphoto.com/id/1203988866/photo/hamburger-with-flying-ingredients.jpg?s=612x612&w=0&k=20&c=_9kctCF9OX33WmIaWLgF8EthWtMZy39hdEY9LZtQHeg=" class="d-block w-100" alt="burger"/>
+
+    <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        <img src="https://zahramediagroup.com/wp-content/uploads/2021/09/Blog-september.jpg" className="d-block w-100" alt="food plate"/>
       </div>
-      <div class="carousel-item active">
-        <img src="" class="d-block w-100" alt=""/>
+      <div className="carousel-item">
+        <img src="https://images.squarespace-cdn.com/content/v1/51f9ecc2e4b0cc5aa44cb93a/1412107808846-K5930U0Z6DXUCFW0EA5Z/_MG_4145.jpg" className="d-block w-100" alt="food"/>
       </div>
-      <div class="carousel-item">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO7M6dNz9B9u_DnrZeAszOVRXK_uji2_MzRjcMCbUc0YZRDFyz1Na19SEK6zVYU1B24so&usqp=CAU" class="d-block w-100" alt="image"/>
+      <div className="carousel-item">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSTnGUbmvNvYjK1ZZMNOj6ory-5S-OG-H5yMwdGiptj6iS2BKAYrOFBtk-VrmWxcRrmxA&usqp=CAU" className="d-block w-100" alt="tasty"/>
+      </div>
+      <div className="carousel-item">
+      <img src="https://expertphotography.b-cdn.net/wp-content/uploads/2020/04/food-photography-blogs-spread.jpg" className="d-block w-100" alt="tasty"/>
       </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
   </div>
+  
   )
 }
 
