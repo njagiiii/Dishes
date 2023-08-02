2489be7e972c7902185054bebc9da9dab0687d95
njagiiii <grace.makena@student.moringaschool.com>
2023-08-01 21:18:24 -0400

    Added comments to follow for navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import './index.css';
+import Navbar from './Navbar';
+
 
 function App() {
   return (
     <div >
-      {/* <h1>Hello World</h1> */}
+      <Navbar />
+     
     </div>
-  );
+  )
 }
-
+  
+    
+ 
 export default App;
