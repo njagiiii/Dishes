ed7f3a9c510f5f096eacccebbb80292ecd1c7546
Noelle <noelle.maingi@student.moringaschool.com>
2023-08-02 10:32:48 +0300

    Added the career component and a paragraph to it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Careers from './Careers';
 import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
+      <Careers />
       <Footer />
     </div>
   );
---
+++ src/Careers.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Careers = () => {
+  return (
+    <div>
+      <p>
+      We are always looking for eager, hard working people with exciting personalities, a passion for superb service and an eye for detail.
+
+We offer flexible hours, hands on training, competitive wages and staff incentives. Tasty Dishes is always accepting applications for all positions and we believe that every person that comes through our front door looking for work deserves a moment of our time.
+
+Please feel free to drop by any of our locations any day of the week between 9:00am and 4:00pm to speak with a manager or just fill in the form below with your data and position you’re applying for.
+      </p>
+    </div>
+  )
+}
+
+export default Careers
