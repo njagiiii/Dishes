aad78ccff3a7831962695d7e5bfdc394d3dab674
Abed Nganga <ngangaabed@gmail.com>
2023-08-02 22:15:11 +0300

    Added Menu Context

---
+++ src/MenuContext.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect, createContext, useContext } from "react";
+
+//Create our context
+const Menu = createContext();
+
+//Creation of a custom hook to use the context
+export const MenuData = () => useContext(Menu);
+
+function MenuContext({ children }) {
+  const [data, setData] = useState({});
+
+  function fetchData () {
+    fetch ('https://tasty-dishes-cr4o.onrender.com/menu')
+    .then((res) => res.json())
+    .then((data) => setData(data))
+  }
+
+  useEffect (() => {
+    fetchData();
+  }, [])
+
+  return <Menu.Provider value={data}> {children} </Menu.Provider>;
+}
+
+export default MenuContext;
