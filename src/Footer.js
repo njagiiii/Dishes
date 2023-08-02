9e55973c1a563e88bf64e7098cf6e11cc6da3238
Noelle <noelle.maingi@student.moringaschool.com>
2023-08-02 10:08:13 +0300

    Footer has the quicklinks, socials and contacts

---
+++ src/Footer.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-light py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3">
+            <h2>Quick links</h2>
+            <ul className="list-unstyled">
+              <li>
+                <a href="/" className="text-decoration-none">About Us</a>
+              </li>
+              <li>
+                <a href="/" className="text-decoration-none">Product</a>
+              </li>
+              <li>
+                <a href="/" className="text-decoration-none">Blog</a>
+              </li>
+              <li>
+                <a href="/" className="text-decoration-none">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-3">
+            <h2>Contacts</h2>
+            <ul className="list-unstyled">
+              <li>
+                <a href="tel:254725531709" className="text-decoration-none">(254)-725-531-709</a>
+              </li>
+              <li>
+                <a href="tel:254722310349" className="text-decoration-none">(254)-722-310-349</a>
+              </li>
+              <li>
+                <a href="mailto:tastydishes.co.ke" className="text-decoration-none">allthings@attech.co.ke</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-3">
+            <h2>Socials</h2>
+            <ul className="list-inline socials">
+              <li className="list-inline-item">
+                <a href="https://facebook.com"><i className="fab fa-facebook-messenger"></i></a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://twitter.com"><i className="fab fa-twitter"></i></a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://google.com"><i className="fab fa-google"></i></a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://youtube.com"><i className="fab fa-youtube"></i></a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://instagram.com"><i className="fab fa-instagram"></i></a>
+              </li>
+            </ul>
+            <h6 className="copy">2023 Tasty Dishes</h6>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import '@fontawesome/fontawesome-free/css/all.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
