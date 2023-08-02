88ebacc29f1060ca8fb08867e3ca6b1e9d773309
Abed Nganga <ngangaabed@gmail.com>
2023-08-02 22:19:36 +0300

    Added Desserts Component

---
+++ src/Dessert.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { MenuData } from './MenuContext';
+
+function Dessert() {
+  const menuData = MenuData();
+
+  // Check if data is available
+  if (!menuData || !menuData.desserts || menuData.desserts.length === 0) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className='container'>
+      <h2 className='text-center mt-4 mb-4'>Desserts Menu</h2>
+      <div className="row">
+        {menuData.desserts.map((item) => (
+          <div key={item.id} className="col-md-4 mb-4">
+            <div className="card h-100">
+              <img
+                src={item.image}
+                alt={item.name}
+                className="card-img-top"
+                style={{ height: '200px', objectFit: 'cover' }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{item.name}</h5>
+                <p className="card-text">{item.description}</p>
+                <p className="card-text">Price: Ksh {item.price}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Dessert;
